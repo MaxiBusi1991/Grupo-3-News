ce7c9f6550e7c89aefd3df0675eaa1964ab60715
nicosalasaraoz <nicolassalasaraoz@gmail.com>
2022-11-23 20:44:45 -0300

    cambiando el componente article

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,27 +1,37 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-// import Articles from "./Articles";
-import ArticlesList from "./ArticlesList";
+import ArticleList from "./ArticleList";
 import Footer from "./Footer";
 import Header from "./Header";
 import NavbarMain from "./NavbarMain";
 import Error404 from "./Error404";
 
+import { Route, Routes } from "react-router-dom";
+import PaginaAdminFeatured from "./PaginaAdminFeatured";
+import PaginaAdminUser from "./PaginaAdminUser";
 const Main = () => {
   return (
     <>
       <Container fluid className="d-flex flex-column min-vh-100 px-0">
         <NavbarMain />
         <Header />
-        {/* <Articles /> */}
-        <ArticlesList />
+        {/* <Articles/> */}
+			<ArticleList />
         <Footer />
       </Container>
       <NavbarMain />
       <Header />
       <Footer />
+
+      <Routes>
+        <Route path="/error404" element={<Error404 />} />
+      </Routes>
+
       <Error404 />
+      <PaginaAdminFeatured />
+      <PaginaAdminUser />
     </>
   );
 };
+
 export default Main;
